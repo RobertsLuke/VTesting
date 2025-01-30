04142ea0940016960895fb9ddedb968b1a4b05a2
JibPython <jibrail.ali.2005@gmail.com>
2025-01-30 10:00:19 +0000

    initalising join.dart state

---
+++ lib/join/join.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BlankPage extends StatelessWidget {
+  const BlankPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: const Center(
+        child: Text(''), // Blank body
+      ),
+    );
+  }
+}
