5c82f69fd9497f4af286f7d1735854ed0635e99b
JibPython <jibrail.ali.2005@gmail.com>
2025-01-30 10:39:58 +0000

    creating functionality of pressing button and retrieves value from input fields

--- lib/join/join.dart
+++ lib/join/join.dart
@@ -8,8 +8,52 @@ class JoinProject extends StatefulWidget {
 }
 
 class _JoinProjectState extends State<JoinProject> {
+
+  // creating controllers so you can retrieve the text value from the input fields
+  final idController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // creating a function that returns a Input Box
+  TextField createInputField(TextEditingController controller, String hintMessage) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+          hintText: hintMessage
+      ),
+    );
+  }
+
+
+  // this function will be called when the user taps the submit button
+  void submitAction() {
+    print(idController.text);
+    print(passwordController.text);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("Join Group"),
+          const Text("Group Name"),
+          /*
+          TextField(
+            controller: idController,
+              decoration: const InputDecoration(
+                hintText: "Enter Group ID"
+              ),
+          ),
+           */
+          createInputField(idController, "Enter Group ID"),
+          const Text("Group Password"),
+          createInputField(passwordController, "Enter Group Password"),
+          ElevatedButton(
+              onPressed: submitAction,
+              child: const Text("Submit"))
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     MaterialApp(
       // setting up initial routes to different screens, loading up login first
-      initialRoute: "/login",
+      initialRoute: "/join",
       routes: {
         "/login": (context) => const LoginScreen(),
         "/join": (context) => const JoinProject()
