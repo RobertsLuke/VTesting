04481a6c0e974878bff0f0be5eac061a686ddb93
JibPython <jibrail.ali.2005@gmail.com>
2025-01-30 11:20:37 +0000

    add basic validation to join project textformfields

--- lib/join/join.dart
+++ lib/join/join.dart
@@ -9,50 +9,72 @@ class JoinProject extends StatefulWidget {
 
 class _JoinProjectState extends State<JoinProject> {
 
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   // creating controllers so you can retrieve the text value from the input fields
   final idController = TextEditingController();
   final passwordController = TextEditingController();
 
   // creating a function that returns a Input Box
-  TextField createInputField(TextEditingController controller, String hintMessage) {
-    return TextField(
+  TextFormField createInputField(TextEditingController controller, String hintMessage, int maxInputLength, Function validatorFunction) {
+    return TextFormField(
       controller: controller,
       decoration: InputDecoration(
-          hintText: hintMessage
+          hintText: hintMessage,
       ),
+      maxLength: maxInputLength,
+      validator: (value) {
+         return validatorFunction(value);
+        },
     );
   }
 
+  // validator function for the group id input field to make sure it meets requirements
+  String? idInputValidator(String value) {
+    if (value.isEmpty) {
+      return "Can not leave this field empty";
+    }
+    return null;
+  }
+
+  // validator function for the password input field to make sure it meets requriements
+  String? passwordInputValidator(String value) {
+    if (value.isEmpty) {
+      return "Can not leave this field empty";
+    }
+    return null;
+  }
 
   // this function will be called when the user taps the submit button
   void submitAction() {
+
+    // validate the form key. Will trigger the validator in the input field's TextFormField
+    if (_formKey.currentState!.validate()) {
+
+    }
     print(idController.text);
     print(passwordController.text);
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text("Join Group"),
-          const Text("Group Name"),
-          /*
-          TextField(
-            controller: idController,
-              decoration: const InputDecoration(
-                hintText: "Enter Group ID"
-              ),
-          ),
-           */
-          createInputField(idController, "Enter Group ID"),
-          const Text("Group Password"),
-          createInputField(passwordController, "Enter Group Password"),
-          ElevatedButton(
-              onPressed: submitAction,
-              child: const Text("Submit"))
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            const Text("Join Group"),
+            const Text("Group Name"),
+            // change the max input length to match specification in user requirements
+            createInputField(idController, "Enter Group ID", 10, idInputValidator),
+            const Text("Group Password"),
+            createInputField(passwordController, "Enter Group Password", 10, passwordInputValidator),
+            ElevatedButton(
+                onPressed: submitAction,
+                child: const Text("Submit"))
+          ],
+        ),
       ),
     );
   }
