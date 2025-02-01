89626bd5ff111a97e5e4bee9e09d6b57aa5acbb9
LagFactory <92254411+LagFactory@users.noreply.github.com>
2025-02-01 13:51:39 +0000

    Update main.dart
    added navigation functionality to move between pages for easier debugging later

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import "package:flutter/material.dart";
+import 'package:sevenc_iteration_two/testingNavigation.dart';
+import 'package:sevenc_iteration_two/userProfile/profile.dart';
+
 import './login/login.dart';
 import './join/join.dart';
 
+
 void main() {
   runApp(
-    MaterialApp(
-      // setting up initial routes to different screens, loading up login first
-      initialRoute: "/login",
-      routes: {
-        "/login": (context) => const LoginScreen(),
-        "/join": (context) => const JoinProject()
-      }
-    )
+      MaterialApp(
+        // setting up initial routes to different screens, loading up login first
+          initialRoute: "/navigation", 
+        // replace the "/navigation" with the page you are testing if you dont want to go through navigation
+          routes: {
+            "/navigation": (context) => const NavigationPage(),
+            "/login/login": (context) => const LoginScreen(),
+            "/join/join": (context) => const JoinProject(),
+            "/userProfile/profile": (context) => const UserProfile(),
+
+          }
+      )
   );
 }
