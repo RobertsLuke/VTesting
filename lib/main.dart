42633031630692f20259477c2a29f9c8a082900d
JibPython <jibrail.ali.2005@gmail.com>
2025-02-03 14:44:06 +0000

    added user in database file and added temp solution for render overflow in project screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:sevenc_iteration_two/testingNavigation.dart';
-import 'package:sevenc_iteration_two/userProfile/profile.dart';
+//import 'package:sevenc_iteration_two/userProfile/profile.dart';
 
 import './login/login.dart';
 import './join/join.dart';
@@ -10,13 +10,13 @@ void main() {
   runApp(
       MaterialApp(
         // setting up initial routes to different screens, loading up login first
-          initialRoute: "/navigation", 
+          initialRoute: "/join",
         // replace the "/navigation" with the page you are testing if you dont want to go through navigation
           routes: {
             "/navigation": (context) => const NavigationPage(),
-            "/login/login": (context) => const LoginScreen(),
-            "/join/join": (context) => const JoinProject(),
-            "/userProfile/profile": (context) => const UserProfile(),
+            "/login": (context) => const LoginScreen(),
+            "/join": (context) => const JoinProject(),
+            // "/userProfile/profile": (context) => const UserProfile(),
 
           }
       )
